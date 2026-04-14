02e4920a2c2f001126703bd3715d01c6b5027472
Gunavathi <gunavathyd93@gmail.com>
2026-04-14 16:03:09 +0530

    added State and Effect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import "./App.css";
 function App() {
-  const name = "guna";
+  const [message, setMessage] =
+  useState("Click to apply");
+
   function handleClick() {
-    alert("submitted");
+    setMessage("submitted");
   }
+  useEffect(() => {
+    console.log("page loaded");
+  }, []);
+  
   return (
     <div>
       <h1>Welcome to my job portal</h1>
-      <p>Hello {name}</p>
+      <p>{message}</p>
       <button className="apply" onClick={handleClick}>Apply now</button>
     </div>
   );
 }
 export default App;
+
